a521e46a7e5c6da730ea9740e1c89d459ab61b55
Arups001 <arups5803@gmail.com>
2026-06-23 13:00:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+// MUSIC
+
 window.addEventListener("load", () => {
 
     const music = document.getElementById("bgMusic");
 
-    music.muted = false;
+    music.volume = 0.7;
 
     const playPromise = music.play();
 
     if (playPromise !== undefined) {
+
         playPromise.catch(() => {
 
-            document.body.addEventListener(
-                "click",
-                () => {
-                    music.play();
-                },
-                { once: true }
-            );
+            document.body.addEventListener("click", () => {
+
+                music.play();
+
+            }, { once: true });
 
         });
     }
+});
+
+
+// FADE ANIMATION
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0px)";
+        }
+
+    });
+
+});
+
+
+document.querySelectorAll(".card,.gallery img,.paper").forEach(el => {
+
+    el.style.opacity = 0;
+    el.style.transform = "translateY(50px)";
+    el.style.transition = "1s";
+
+    observer.observe(el);
 
 });
